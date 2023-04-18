efac17f36befa226e11aa46ed1e0884806e8f8c8
Victor Martinez <victor.martinez@lend.tech>
2023-04-17 21:20:23 -0300

    feat: basic usage of tracer

--- myfastapi/myfastapi/api/main.py
+++ myfastapi/myfastapi/api/main.py
@@ -1,6 +1,21 @@
 from typing import Any, Dict
 
 from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer(__name__)
 
 
 def create_application() -> FastAPI:
@@ -18,10 +33,12 @@ def configure_routes(app: FastAPI) -> None:
 def configure_healthcheck(app: FastAPI) -> None:
     @app.get("/")
     async def healthcheck() -> Dict[str, Any]:
-        return {
-            "application": "My FastAPI",
-            "healthy": True,
-        }
+        with tracer.start_as_current_span("healthcheck") as span:
+            span.set_attribute("application.name", "my fastapi")
+            return {
+                "application": "My FastAPI",
+                "healthy": True,
+            }
 
 
 app = create_application()
