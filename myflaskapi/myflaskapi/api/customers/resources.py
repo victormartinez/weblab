b2d3a13c65f5e92ff38a59265c80aade98b3b6d9
Victor Martinez <victor.martinez@lend.tech>
2023-04-24 10:14:24 -0300

    chore: configure basic trace for customer list

--- myflaskapi/myflaskapi/api/customers/resources.py
+++ myflaskapi/myflaskapi/api/customers/resources.py
@@ -3,20 +3,25 @@ from uuid import UUID
 
 from flask import Blueprint
 from flask_pydantic import validate
+from opentelemetry import trace
 
 from .data import CUSTOMERS
 from .schema import CustomerOut
 
 app = Blueprint("customers", __name__)
+tracer = trace.get_tracer(__name__)
 
 
 @app.route("/customers", methods=["GET"])
 @validate()
 def list() -> List[CustomerOut]:
-    return [
-        CustomerOut(id=idx, **data).dict()
-        for idx, data in CUSTOMERS.items()
-    ]
+    with tracer.start_as_current_span("list-customers") as span:
+        result = [
+            CustomerOut(id=idx, **data).dict()
+            for idx, data in CUSTOMERS.items()
+        ]
+        span.set_status(trace.Status(trace.StatusCode.OK))
+        return result
 
 
 @app.route("/customers/<id>", methods=["GET"])
--- myflaskapi/myflaskapi/api/ext.py
+++ myflaskapi/myflaskapi/api/ext.py
@@ -1,7 +1,20 @@
 from typing import Any, Callable, Dict, Union
 
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
 
 
 def configure_extensions(app: Flask) -> None:
-    pass
+    _configure_telemetry(app)
+
+
+def _configure_telemetry(_: Flask):
+    provider = TracerProvider()
+    processor = BatchSpanProcessor(ConsoleSpanExporter())
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
